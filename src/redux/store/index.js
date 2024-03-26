01e58079025abde8f8c9044bad6814428b83599d
GiuliaLanari <giulia.lanari96@gmail.com>
2024-03-26 18:55:51 +0100

    add presist

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,14 +2,29 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
 import favourites from "../reducers/favourites";
 import resoultsJobSearch from "../reducers/resoultsJobSearch";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const unifiedReducer = combineReducers({
   job: favourites,
   search: resoultsJobSearch,
 });
 
-const store = configureStore({
-  reducer: unifiedReducer,
-});
+const persistedReducer = persistReducer(persistConfig, unifiedReducer);
 
-export default store;
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
+});
+const persistor = persistStore(store);
+
+export { store, persistor };
